283417cee7648e58ab569c177fc01fe01245b277
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-22 20:06:17 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
-import "./styles.css";
+/*Tipos de Números en Arreglo:
+Almacene en un arreglo de dimensión N números (la cantidad es ingresada por el usuario)
+Muestre cuántos números son positivos, cuántos son negativos y cuántos ceros hay
+*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+
+// declaro una Funciones para solicitar el valor y validad el dato ingresado o pedirle de nuevo
+function isnumber (position:number):number
+  {
+    let itsnotnum: boolean = true; 
+
+    while (itsnotnum) 
+      {
+      let num: number = Number(prompt("Ingrese un número de la posición " + position));
+      if (isNaN(num) || num % 1) 
+        {
+        alert("Solos se adminten un números enteros");
+        } else {
+          itsnotnum = false;
+          return num;
+          }
+      }
+  }
+
+//Empiezo con mi programa main
+console.clear();
+let longvector :number = Number(prompt("Ingrese el largo del vector "));
+let vector:number[] = new Array (longvector);
+let countneg:number=0;
+let countpos:number=0;
+let countzero:number=0;
+
+for (let i: number = 0; i <longvector; i++)                                //Cargo los datos y comparo
+  {
+    vector[i]=isnumber(i+1);
+    if (vector[i]===0) 
+    {
+      countzero++;
+    } else {
+            if (vector[i]>0)
+              {
+               countpos++;   
+              } else {
+                      countneg++;
+                      }
+
+            }
+  }
+console.log("Hay: " + countpos + " números positivos, " + countneg + " números negativos y " + countzero + " Ceros");
