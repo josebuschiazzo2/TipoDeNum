edb1b8531622235af1da6aa93320864d2a3c48fc
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-22 22:11:53 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,26 @@ Muestre cuántos números son positivos, cuántos son negativos y cuántos ceros
 function isnumber (position:number):number
   {
     let itsnotnum: boolean = true; 
-
+    let num: number=0;
     while (itsnotnum) 
       {
-      let num: number = Number(prompt("Ingrese un número de la posición " + position));
-      if (isNaN(num) || num % 1) 
-        {
-        alert("Solos se adminten un números enteros");
-        } else {
-          itsnotnum = false;
-          return num;
-          }
+      
+        if (position<0) {
+          num = Number(prompt("Ingrese el largo del vector "));
+          } else {
+              num = Number(prompt("Ingrese un número de la posición " + position));
+              }
+      if (isNaN(num) || num % 1){
+                alert("Solos se adminten un números enteros");
+                } else {
+                  itsnotnum = false;
+                  return num;
+                  }
       }
   }
-
 //Empiezo con mi programa main
 console.clear();
-let longvector :number = Number(prompt("Ingrese el largo del vector "));
+let longvector: number = isnumber(-1);
 let vector:number[] = new Array (longvector);
 let countneg:number=0;
 let countpos:number=0;
